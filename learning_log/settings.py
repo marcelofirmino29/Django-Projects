1c1a29befd3ddf3f3a3812869b46023f33d29c4d
marcelofirmino29 <marc.firmino@gmail.com>
2024-07-22 14:48:42 -0400

    Funcional para linux

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -126,6 +126,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CRISPY_ALLOWED_TEMPLATE_PACKS = ['bootstrap4']
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 #url login page
